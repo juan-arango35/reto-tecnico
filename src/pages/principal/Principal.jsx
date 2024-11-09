ee1d3fcef93522ee130dd743be10b3b96c882331
juan arango <juanarangozapata35@gmail.com>
2024-11-09 15:48:10 -0500

    centrando input

--- src/pages/principal/Principal.jsx
+++ src/pages/principal/Principal.jsx
@@ -22,7 +22,7 @@ const Principal = () => {
       const saveState = JSON.parse(
         sessionStorage.getItem("estadoComponentePrincipal")
       );
-      console.log(saveState)
+      console.log(saveState);
       if (saveState) {
         setSuccessRecords(saveState.successRecords);
         setErrorRecords(saveState.errorRecords);
@@ -151,70 +151,72 @@ const Principal = () => {
   }
 
   return (
-    <div className="min-h-screen bg-white p-4">
-      <h1 className="bg-white h-36 flex justify-center items-center text-3xl mb-6">
-        Sistema de Carga de Datos
-      </h1>
-      <hr className="mb-6" />
+    <div className="flex justify-center">
+      <div className="min-h-screen bg-white p-4">
+        <h1 className="bg-white h-36 flex justify-center items-center text-3xl mb-6">
+          Sistema de Carga de Datos
+        </h1>
+        <hr className="mb-6" />
 
-      {showForm && (
-        <form
-          onSubmit={handleSubmit}
-          className="mb-6 bg-white p-4 rounded-lg shadow-md"
-        >
-          <div className="mb-4">
-            <input
-              type="file"
-              accept=".csv"
-              onChange={handleFileChange}
-              className="block w-full text-sm text-gray-500
+        {showForm && (
+          <form
+            onSubmit={handleSubmit}
+            className="mb-6 bg-white p-4 rounded-lg shadow-md"
+          >
+            <div className="mb-4">
+              <input
+                type="file"
+                accept=".csv"
+                onChange={handleFileChange}
+                className="block w-full text-sm text-gray-500
                 file:mr-4 file:py-2 file:px-4
                 file:rounded-full file:border-0
                 file:text-sm file:font-semibold
                 file:bg-blue-50 file:text-blue-700
                 hover:file:bg-blue-100"
+              />
+            </div>
+            <button
+              type="submit"
+              disabled={isLoading || !file}
+              className={`px-4 py-2 font-bold text-white rounded ${
+                isLoading || !file
+                  ? "bg-gray-300 cursor-not-allowed"
+                  : "bg-blue-500 hover:bg-blue-700"
+              }`}
+            >
+              {isLoading ? "Procesando..." : "Cargar CSV"}
+            </button>
+          </form>
+        )}
+
+        {error && <p className="text-red-500 mb-4">{error}</p>}
+
+        {(successRecords.length > 0 || errorRecords.length > 0) && (
+          <div className="flex justify-center flex-col items-center">
+            <ResultDisplay
+              successRecords={successRecords}
+              clearRecords={clearRecords}
+              showFormFn={showFormFn}
+            />
+            <ErrorCorrection
+              errorRecords={errorRecords}
+              handleRetry={handleRetry}
             />
           </div>
-          <button
-            type="submit"
-            disabled={isLoading || !file}
-            className={`px-4 py-2 font-bold text-white rounded ${
-              isLoading || !file
-                ? "bg-gray-300 cursor-not-allowed"
-                : "bg-blue-500 hover:bg-blue-700"
-            }`}
-          >
-            {isLoading ? "Procesando..." : "Cargar CSV"}
-          </button>
-        </form>
-      )}
-
-      {error && <p className="text-red-500 mb-4">{error}</p>}
-
-      {(successRecords.length > 0 || errorRecords.length > 0) && (
-        <div className="flex justify-center flex-col items-center">
-          <ResultDisplay
-            successRecords={successRecords}
-            clearRecords={clearRecords}
-            showFormFn={showFormFn}
-          />
-          <ErrorCorrection
-            errorRecords={errorRecords}
-            handleRetry={handleRetry}
-          />
-        </div>
-      )}
-
-      {!showForm &&
-        successRecords.length === 0 &&
-        errorRecords.length === 0 && (
-          <button
-            onClick={showFormFn}
-            className="px-4 py-2 font-bold text-white rounded bg-blue-500 hover:bg-blue-700"
-          >
-            Cargar nuevo archivo
-          </button>
         )}
+
+        {!showForm &&
+          successRecords.length === 0 &&
+          errorRecords.length === 0 && (
+            <button
+              onClick={showFormFn}
+              className="px-4 py-2 font-bold text-white rounded bg-blue-500 hover:bg-blue-700"
+            >
+              Cargar nuevo archivo
+            </button>
+          )}
+      </div>
     </div>
   );
 };
